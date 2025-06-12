422910313536738b5ec51e593a2f4ae495496201
meghana610 <devatameghana610@gmail.com>
2025-06-12 12:19:01 +0530

    practice

---
+++ func.js
@@ -0,0 +1,45 @@
+// Functions are one of the most powerful features in javascript
+// They allow you to reuse code, organize logic, and handle dynamic inputs.
+
+// Functions are used always like
+// declaration + call
+
+// syntax
+// Function declaration
+
+// sayHello()
+
+// function sayHello() {
+//     console.log("Hello")
+// }
+
+// // function calling
+// sayHello()
+// sayHello()
+// // this is called js hosting
+
+// let a = 10;
+// console.log(a)
+
+// functions with parameters
+
+function sum(num1, num2, num3){
+    console.log(num1 + num2 + num3)
+}
+sum(10, 5, 10)
+sum(5, 5, 100)
+
+function sub(num1, num2, num3){
+    console.log(num1 - num2 - num3)
+}
+
+function multiple(num1, num2, num3){
+    console.log(num1 * num2 * num3)
+}
+
+function multiple(a, b, c){
+    console.log(a * b * c)
+}
+
+multiple(100, 50, 10)
+multiple(1, 2, 3)
